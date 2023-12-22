ee4915ed1ecf82381cc3b03d8b2eb00701322e16
nojood2023 <151244570+nojood2023@users.noreply.github.com>
2023-12-22 11:42:21 +0300

    Add files via upload

---
+++ stackView/stackView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stackView
+//
+//  Created by Nojood Aljuaid  on 07/06/1445 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
